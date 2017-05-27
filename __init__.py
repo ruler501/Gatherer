a49357447844fa4a8443188943d7390e9175dcea
Devon Richards <devon@apptimize.com>
2017-05-26 23:05:52 -0500

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from gatherer import get_color_identity, get_all_printings, get_name, import_dec, \
+                     export_dec, import_coll2, export_coll2, get_card
---
+++ cards.py
@@ -0,0 +1,4 @@
+from gatherer import get_card
+
+for i in range(1, 416917):
+    print(get_card(i))
